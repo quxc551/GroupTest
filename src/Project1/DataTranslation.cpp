93d593c6de876bca03fa6fbc0a1e0a97cca698a7
quxc551 <60848379+quxc551@users.noreply.github.com>
2020-03-24 20:22:05 +0800

    更新

--- src/Project1/DataTranslation.cpp
+++ src/Project1/DataTranslation.cpp
@@ -1,26 +1,34 @@
 #include"DataTranslation.h"
+int DataTranslator::Parity = 0;
+
+DataTranslator::DataTranslator(string a, string b)
+{
+	outfile1 = a;
+	outfile2 = b;
+}
 
 bool DataTranslator::WhetherInArea(int x, int y)
 {
-	if ((x > 7 && x < (dx - 8) && y >= 0 && y < dy) || (y > 7 && y < (dy - 8) && x >= 0 && x <= dx))
-		return 1;
-	else
-	{
-		if (x >= (dx - 8) && y >= (dy - 8) && x < dx&&y < dy)
-		{
-			if (x > (dx - 7) && x<(dx - 1) && y>(dy - 7) && y < (dy - 1))
-				return 0;
-			else
-				return 1;
-		}
+
+		if ((x > 7 && x < (dx - 8) && y >= 0 && y < dy) || (y > 7 && y < (dy - 8) && x >= 0 && x <= dx))
+			return 1;
 		else
-			return 0;
-	}
+		{
+			if (x >= (dx - 8) && y >= (dy - 8) && x < dx&&y < dy)
+			{
+				if (x > (dx - 7) && x<(dx - 1) && y>(dy - 7) && y < (dy - 1))
+					return 0;
+				else
+					return 1;
+			}
+			else
+				return 0;
+		}
+	
 }
 
 int DataTranslator::getnextD()
 {
-
 	while (WhetherInArea(nowx, nowy) == 0)
 	{
 		if (nowy >= dy)
@@ -28,20 +36,22 @@ int DataTranslator::getnextD()
 			nowy--;
 			nowx++;
 			direction = -1;
+			
 		}
 		else if (nowy < 0)
 		{
 			nowy++;
 			nowx++;
 			direction = 1;
+		
 		}
 		else
 		{
 			nowy += direction;
 		}
-	}
+	}	
 	int j = DataArray[nowx][nowy];
-	nowy += direction;
+	nowy+=direction;
 	return j;
 }
 
@@ -59,93 +69,369 @@ short DataTranslator::InternetCheckSum(unsigned short*buf, int nwords)
 	{
 		sum = (sum >> 16) + sum & 0xffff;
 	}
+	cout << sum << endl;
 	return ~sum;
 }
-void DataTranslator::Initialization(unsigned short**tes)
+
+
+bool DataTranslator::Check_checkCRC8(unsigned char* pdat, unsigned char len)
 {
-	DataArray = tes;
-	if (DataArray[0][8] == 1)//读入两个bit
-		Parity = 0;
-	else
-		Parity = 1;
-	int i = 16;
-	nowx = 0, nowy = 10;
-	while (i != 0)//读入16个bit 对Bytnum初始化
+	unsigned int i;
+	unsigned char j, sum;
+	unsigned char crc = 0x00;
+	for (i = 0; i < len; i++) {
+		crc ^= ((*pdat++) & 0xFF);
+		for (int j = 8; j > 0; j--)
+		{
+			if (crc & 0x80) {
+				crc <<= 1;
+				crc ^= 0x31;
+			}
+			else { crc <<= 1; }
+		}
+	}
+	sum = *pdat;
+	if (sum == crc)
 	{
-		int j = getnextD();
-		//cout << j;
-		if (j == 1)
-			ByteNum = (ByteNum << 1) | 1;
-		else ByteNum = ByteNum << 1;
-		i--;
+		//cout << 0 << endl;
+		allbyte++;
+		return true;
+	}
+	else
+	{
+		//cout << 1 << endl;
+		allbyte++;
+		error++;
+		return false;
+	}
+
+}
+
+
+unsigned char*DataTranslator::hanmingcode(unsigned char*ch, bool &s)
+{
+	bitset<128> checkedData;
+	for (int i = 0; i < 16; ++i)
+	{
+		for (int j = 0; j < 8; ++j)
+		{
+			checkedData[i * 8 + j] = (ch[i] >> (7 - j)) & 1;
+		}
+	}
+	bitset<7> checkPosition;
+	for (int i = 1; i < 128; ++i)
+	{
+		if (i & 1)checkPosition[0] = checkPosition[0] ^ checkedData[i];
+		if (i & 2)checkPosition[1] = checkPosition[1] ^ checkedData[i];
+		if (i & 4)checkPosition[2] = checkPosition[2] ^ checkedData[i];
+		if (i & 8)checkPosition[3] = checkPosition[3] ^ checkedData[i];
+		if (i & 16)checkPosition[4] = checkPosition[4] ^ checkedData[i];
+		if (i & 32)checkPosition[5] = checkPosition[5] ^ checkedData[i];
+		if (i & 64)checkPosition[6] = checkPosition[6] ^ checkedData[i];
+	}
+	allbyte++;
+	int x = checkPosition.to_ulong();
+	if (x)
+	{
+		s = 1;
+		checkedData[x] = ~checkedData[x];
+		error++;
+	}
+	unsigned char data[15];
+	memset(data, 0, 15);
+	for (int i = 1, skip = 0; i < 128; i++)
+	{
+		if (i == 1 || i == 2 || i == 4 || i == 8 || i == 16 || i == 32 || i == 64)
+		{
+			++skip;
+			continue;
+		}
+		data[(i - skip - 1) / 8] += checkedData[i] << (7 - (i - skip - 1) % 8);
+	}
+	cout << x << endl;
+	return data;
+}
+
+void DataTranslator::Initialization(char**tes)
+{
+	//判断奇偶是否符合
+	DataArray = tes;
+	if ((DataArray[0][8]==0&&DataArray[0][9]==7)||(DataArray[0][8]==7&&DataArray[0][9]==0))
+	{
+		if ((DataArray[0][8] == 7) && (Parity == 0))
+		{
+			Parity = 1;
+			PictureChange = 1;
+		}
+		else if ((DataArray[0][8] == 0) && (Parity == 1))
+		{
+			Parity = 0;
+			PictureChange = 1;
+		}
+		else
+			PictureChange = 0;
+
+	}
+	else
+	{
+		PictureChange=0;
+	}
+
+
+	if (PictureChange)
+	{
+
+
+		int i = 16;
+		nowx = 0, nowy = 10;
+		ByteNum = 0;
+		direction = 1;
+		while (i != 0)//读入16个bit 对Bytnum初始化
+		{
+			int j = getnextD();
+			if (j == 0)//黑色 01
+			{
+				ByteNum = ByteNum << 1;
+				ByteNum = (ByteNum << 1) | 1;
+			}
+				
+			else if (j == 2)//绿色 11
+			{
+				ByteNum = (ByteNum << 1) | 1;
+				ByteNum = (ByteNum << 1) | 1;
+			}
+			else if (j == 4)//红色 00
+			{
+				ByteNum = ByteNum << 1;
+				ByteNum = ByteNum << 1;
+			}
+			else if (j == 7)//白色10
+			{
+				ByteNum = (ByteNum << 1) | 1;
+				ByteNum = ByteNum << 1;
+
+			}
+			i-=2;
+		}
 	}
 }
 
-void DataTranslator::OutPutData(unsigned char*data)
+void DataTranslator::OutPutData(unsigned char*data,unsigned char*check,int length)
 {
-	ofstream outFIle("1.dat", ios::app | ios::binary);
-	for (int i = 0; i < sum; i++)
+	ofstream outFIle(outfile1, ios::app | ios::binary);
+	ofstream checkFIle(outfile2, ios::app | ios::binary);
+	for (int i = 0; i < length; i++)
 	{
-		cout << data[i] << " ";
 		outFIle.write((const char*)&data[i], sizeof(char));
+		checkFIle.write((const char*)&check[i], sizeof(char));
 	}
 	outFIle.close();
+	checkFIle.close();
 }
 
-void DataTranslator::DateTranslate(unsigned short**tes)
+void DataTranslator::DateTranslate(char**tes,unsigned char*out,unsigned char*vout)
 {
-	Initialization(tes);
-	unsigned char* test = new unsigned char[ByteNum];//单张图片的数据
-	unsigned char* DataCheckArray = new unsigned char[20];
-	int CheckArrayIndex = 0;
-	int BitNums = ByteNum * 8;
-	int SingleDataCodes;//每一组数据码长度；
-	int SingleErrorCodes;//每一组纠错码长度
-	int testindex = 0;
-	while (BitNums != 0)
+	if (!tes)
 	{
-		SingleDataCodes = 144;
-		SingleErrorCodes = 16;
-		CheckArrayIndex = 0;
-		while (SingleDataCodes != 0)
+		Parity = Parity ^ 1;
+		unsigned char a[466 * 10] = { 0 };
+		unsigned char data[466 * 10] = { 0 };
+		sum = 466 * 10;
+		allbyte += 466;
+		error += 466;
+		for (int i = 0; i < 4660; i++)
 		{
-			for (int i = 0; i < 8; i++)
-			{
-				int j = getnextD();
-				if (j == 1)
-					DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
-				else DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
-			}
-			CheckArrayIndex++;
-			BitNums -= 8;
-			SingleDataCodes -= 8;
+			out[testindex] = 0;
+			vout[testindex] = 0;
+			testindex++;
 		}
-		while (SingleErrorCodes != 0)
-		{
-			for (int i = 0; i < 8; i++)
-			{
-				int j = getnextD();
-				if (j == 1)
-					DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
-				else DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
-			}
-			CheckArrayIndex++;
-			BitNums -= 8;
-			SingleErrorCodes -= 8;
-		}
-		if (InternetCheckSum((unsigned short*)DataCheckArray, 20) == 1)//进行检验
-		{
-			for (int i = 0; i < 18; i++)
-			{
-
-				test[testindex] = DataCheckArray[i];
-
-				testindex++;
-			}
-		}
-
-
+		//OutPutData(a, data);
 	}
-	sum = testindex;
-	OutPutData(test);
+	else
+	{
+
+		int now = error;
+		Initialization(tes);
+		if (PictureChange)
+		{
+			bool sign = 0;
+			if (ByteNum < 20504)
+			{
+				sign = 1;
+			}
+			unsigned char* test = new unsigned char[ByteNum / 4];//单张图片的数据
+			unsigned char* check = new unsigned char[ByteNum / 4];//单张图片的数据
+			unsigned char* DataCheckArray = new unsigned char[11];
+			memset(DataCheckArray, 0, 11);
+			//for (int i = 0; i < 11; i++)
+			//{
+			//	DataCheckArray[i] = '\0';
+			//}
+			int CheckArrayIndex = 0;
+			int BlockNums = ByteNum;
+			while (BlockNums != 0)
+			{
+				while (BlockLength != 0)
+				{
+					TemporaryArray[TemporaryIndex] = getnextD();
+					BlockLength--;
+					TemporaryIndex++;
+					BlockNums--;
+					if (BlockNums == 0)
+					{
+						break;
+					}
+				}
+				if (BlockLength == 0)
+				{
+					memset(DataCheckArray, 0, 11);
+				/*	for (int i = 0; i < 11; i++)
+					{
+						DataCheckArray[i] = '\0';
+					}*/
+					int ArrayIndex = 0;
+
+					for (int i = 44; i > 0;)
+					{
+
+						for (int j = 0; j < 4; j++)
+						{
+
+							int m = TemporaryArray[ArrayIndex];
+							ArrayIndex++;
+							if (m == 0)//黑色 01
+							{
+								DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
+								DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
+							}
+
+							else if (m == 2)//绿色 11
+							{
+								DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
+								DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
+							}
+							else if (m == 4)//红色 00
+							{
+								DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
+								DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
+							}
+							else if (m == 7)//白色10
+							{
+								DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
+								DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
+
+							}
+
+						}
+						CheckArrayIndex++;
+						i -= 4;
+					}
+
+					//进行校验
+					bool checkerror = Check_checkCRC8(DataCheckArray, 10);
+					if (checkerror)
+					{
+						for (int i = 0; i < 10; i++)
+						{
+							out[testindex] = DataCheckArray[i];
+							vout[testindex] = 255;
+						/*	test[testindex] = DataCheckArray[i];
+							check[testindex] = 255;*/
+							testindex++;
+						}
+					}
+					else
+					{
+						for (int i = 0; i < 10; i++)
+						{
+							out[testindex] = DataCheckArray[i];
+							vout[testindex] = 0;
+						/*	test[testindex] = DataCheckArray[i];
+							check[testindex] = 0;*/
+							testindex++;
+						}
+					}
+					BlockLength = 44;
+					TemporaryIndex = 0;
+					CheckArrayIndex = 0;
+					memset(DataCheckArray, 0, 11);
+					//for (int i = 0; i < 11; i++)
+					//{
+					//	DataCheckArray[i] = '\0';
+					//}
+				}
+				if (BlockLength != 0 && sign)
+				{
+					for (int i = 0; i < 11; i++)
+					{
+						DataCheckArray[i] = '\0';
+					}
+					int ArrayIndex = 0;
+					for (int i = TemporaryIndex; i > 0; )
+					{
+						for (int j = 0; j < 4; j++)
+						{
+							int m = TemporaryArray[ArrayIndex];
+							ArrayIndex++;
+							if (m == 0)//黑色 01
+							{
+								DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
+								DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
+							}
+
+							else if (m == 2)//绿色 11
+							{
+								DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
+								DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
+							}
+							else if (m == 4)//红色 00
+							{
+								DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
+								DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
+							}
+							else if (m == 7)//白色10
+							{
+								DataCheckArray[CheckArrayIndex] = (DataCheckArray[CheckArrayIndex] << 1) | 1;
+								DataCheckArray[CheckArrayIndex] = DataCheckArray[CheckArrayIndex] << 1;
+
+							}
+							--i;
+							if (i == 0)
+							{
+								break;
+							}
+						}
+						CheckArrayIndex++;
+					}
+					for (int i = 0; i < CheckArrayIndex; i++)
+					{
+						out[testindex] = DataCheckArray[i];
+						vout[testindex] = 255;
+			/*			test[testindex] = DataCheckArray[i];
+						check[testindex] = 255;*/
+						testindex++;
+					}
+				}
+			}
+			sum = testindex;
+			//now = error - now;
+			//cout << '\t' << now;
+			//OutPutData(test,check);
+		}
+	}
+}
+
+int DataTranslator::getall()
+{
+	return allbyte;
+}
+
+int DataTranslator::geterror()
+{
+	return error;
+}
+
+int DataTranslator::getSum()
+{
+	return testindex;
 }
--- src/Project1/源.cpp
+++ src/Project1/源.cpp
@@ -4,39 +4,104 @@
 #include "slicer.h"
 #include "FileToVedio.h"
 #include "FileToMp4.h"
+#include <bitset>
+#include "DataTranslation.h"
 using namespace std;
 
 using namespace cv; //opencv 的命名空间
 
 
-int main()
+int main(int argc, char* argv[])
 {
-	FileToVedio a(R"(D:\test.txt)");
-	a.GenerateVedio(R"(D:\QR_Code\QR_Code.mp4)", 1080, 1920, 10);
+	static unsigned char temporary[3][10485760] = { 0 };
+	static unsigned char checktemporary[3][10485760] = { 0 };
+	int range[3];
+	/*FileToVedio a(R"(C:\Users\yuyua\Desktop\e1.bin)");
+	a.GenerateVedio(R"(D:\QR_Code\QR_Code.mp4)", 1080, 1920, 10);*/
 	
 	/*FileToMp4 a;
 	a.MakePictureSequence();*/
-	int start = getTickCount();   
-
+	int start = getTickCount();
 	Mat imgGray, res;
 	Mat img;
-	VedioSlicer DT(R"(C:\Users\yuyua\Desktop\录制视频.mp4)");
-	int i = 0;
-	while (1)
+	int count = 1;
+	string inputfile = argv[count++];
+	string outfile1 = argv[count++];
+	string outfile2 = argv[count++];
+	//VedioSlicer GT(inputfile);
+
+
+	int ST = 0;
+	int error = 0;
+	int all = 0;
+	for (int j = 0; j < 3; j++)
 	{
-		// cout << ++i;
-		img = DT.GetNextFrame();
-		// img = imread(".//1.jpg");
-		if (img.empty())
+		string a = "";
+		string b = "";
+		DataTranslator D(a, b);
+		VedioSlicer GT(inputfile);
+		int i = 0;
+		while (1)
 		{
-			break;
+			++i;
+
+			img = GT.GetNextFrame();
+			if (i % 3 != j)continue;
+			if (img.empty())
+			{
+				break;
+			}
+			Detector d(img, 108, 192, 10);
+			if (!d.Detect())throw;
+			char x[2];
+			d.picCount(x);
+			D.DateTranslate(d.GetBinaryData(),temporary[j],checktemporary[j]);
 		}
-		//imwrite(".//1.jpg", res);
-		Detector d(img, 108, 192, 10);
-		if (!d.Detect())throw;
-		d.GetBinaryData();
+		range[j] = D.getSum();
+		int nowall = D.getall();
+		int nowerror = D.geterror();
+		//cout << nowall << " " << nowerror<<endl;
+		if (nowall > all)
+		{
+			ST = j;
+			all = nowall;
+			error = nowerror;
+		}
+		else if (nowall == all)
+		{
+			if (error > nowerror)
+			{
+				ST = j;
+				all = nowall;
+				error = nowerror;
+			}
+		}
 	}
 
+	DataTranslator DT(outfile1, outfile2);
+	DT.OutPutData(temporary[ST], checktemporary[ST], range[ST]);
+	cout << "error:" << error << endl << "all:" << all << endl;
+	//int i = 0;
+	//while (1)
+	//{
+	//	++i;
+	//	img = DS.GetNextFrame();
+	//	if (i % 3 != ST)continue;
+	//	//cout << i << ' ';
+	//	//img = imread(R"(D:\QR_Code\Code10_trans.png)");
+	//	if (img.empty())
+	//	{
+	//		break;
+	//	}
+	//	//imwrite(".//1.jpg", res);
+ //		Detector d(img, 108, 192, 10);
+	//	if (!d.Detect())throw;
+	//	char x[2];
+	//	d.picCount(x);
+	//	//cout << (int)x[0] << (int)x[1] << endl;
+	//	DT.DateTranslate(d.GetBinaryData());
+	//}
+	//DT.out();
 	int end = getTickCount();
 	int last = end - start;
 	cout << "time consume: " << last / getTickFrequency() << 's' << endl;
